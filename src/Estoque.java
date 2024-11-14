15dccde293a7aa9cd924d5ea852a23ea693c95b5
Alessandra <alessandravitoria799@gmail.com>
2024-11-13 21:29:52 -0300

    commit3

--- src/Estoque.java
+++ src/Estoque.java
@@ -21,5 +21,4 @@ public class Estoque {
         produtos.add(produto);
     }
 
-
 }
--- src/Produto.java
+++ src/Produto.java
@@ -1,25 +1,25 @@
 
 public class Produto {
-        private String nome;
-        private double peso;
-        private int valor;
+    private String nome;
+    private double peso;
+    private int valor;
 
-        public Produto(String nome, double peso, int valor){
-            this.nome = nome;
-            this.peso = peso;
-            this.valor = valor;
-        }
+    public Produto(String nome, double peso, int valor){
+        this.nome = nome;
+        this.peso = peso;
+        this.valor = valor;
+    }
 
-        public String getNome(){
-            return nome;
-        }
+    public String getNome(){
+        return nome;
+    }
 
-        public double getPeso(){
-            return peso;
-        }
+    public double getPeso(){
+        return peso;
+    }
 
-        public int getValor(){
-            return valor;
+    public int getValor(){
+        return valor;
     }
 
 }
