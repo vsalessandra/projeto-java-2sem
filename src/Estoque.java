2bfb4fb42cd364f7e26b84ff18677ebb59d8865e
Alessandra <alessandravitoria799@gmail.com>
2024-11-13 21:16:08 -0300

    "commit1"

---
+++ src/Estoque.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Estoque {
+    private int capacidade;
+    private ArrayList<Produto> produtos;
+
+    public Estoque(int capacidade){
+        this.capacidade = capacidade;
+        this.produtos = new ArrayList<>();
+    }
+
+    public int getCapacidade(){
+        return capacidade;
+    }
+
+    public ArrayList<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void adicionarProdutos(Produto produto){
+        produtos.add(produto);
+    }
+
+
+}
